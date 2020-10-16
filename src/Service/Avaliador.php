ec2aecf7c0d3bad8cb627cfe5608e3f863bcbcc7
Cezar Rosa <cecez86@gmail.com>
2020-10-16 10:43:10 -0300

    * obtendo menor valor do leilão * obtendo 3 maiores lances do leilão * testes para menor valor do leilão

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -4,30 +4,60 @@
 namespace Alura\Leilao\Service;
 
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
 
-    private int $_maiorValor = -INF;
+    private $_maiorValor = -INF;
+    private $_menorValor = INF;
+    private $_maioresLances;
 
-    public function avalia(Leilao $leilao) : void
+    public function avalia(Leilao $leilao): void
     {
-
+        // laço para obter maior e menor valor
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->_maiorValor) {
                 $this->_maiorValor = $lance->getValor();
             }
+
+            if ($lance->getValor() < $this->_menorValor) {
+                $this->_menorValor = $lance->getValor();
+            }
         }
+
+        // código para retornar primeiros 3 lances
+        $lances = $leilao->getLances();
+        usort($lances, function(Lance $lance1, Lance $lance2) {
+            return $lance1->getValor() - $lance2->getValor();
+        });
+        $this->_maioresLances = array_slice($lances, 0, 3);
 
     }
 
     /**
-     * @return mixed
+     * @return float
      */
-    public function getMaiorValor() : float
+    public function getMaiorValor(): float
     {
         return $this->_maiorValor;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMenorValor(): float
+    {
+        return $this->_menorValor;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->_maioresLances;
     }
 
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -74,5 +74,67 @@ class AvaliadorTest extends TestCase
         $this->assertEquals(2400, $avaliador->getMaiorValor());
 
     }
+
+    public function testAvaliadorMenorValorComLancesEmOrdemCrescente()
+    {
+        // prepara, arruma a casa para os testes
+        // arrange - given
+
+        // cria usuários
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        // cria leilão
+        $leilao = new Leilao('Fiat 147 0km');
+
+        // cria lances
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($joao, 2400));
+
+        // retorna avaliador
+        $avaliador = new Avaliador();
+
+        // executa o que se deseja testar
+        // act - when
+
+        $avaliador->avalia($leilao);
+
+
+        // avalia-se o resultado
+        // assert - then
+        $this->assertEquals(2000, $avaliador->getMenorValor());
+
+    }
+
+    public function testAvaliadorMenorValorComLancesEmOrdemDecrescente()
+    {
+        // prepara, arruma a casa para os testes
+        // arrange - given
+
+        // cria usuários
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        // cria leilão
+        $leilao = new Leilao('Fiat 147 0km');
+
+        // cria lances
+        $leilao->recebeLance(new Lance($joao, 2400));
+        $leilao->recebeLance(new Lance($maria, 2000));
+
+        // retorna avaliador
+        $avaliador = new Avaliador();
+
+        // executa o que se deseja testar
+        // act - when
+
+        $avaliador->avalia($leilao);
+
+
+        // avalia-se o resultado
+        // assert - then
+        $this->assertEquals(2000, $avaliador->getMenorValor());
+
+    }
     
 }
